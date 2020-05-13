1d3993b8ad5c27623a497e9d948624fc547b1da0
Daniel Okoro <danielochinasa@gmail.com>
2020-05-13 12:24:18 +0100

    Updates

---
+++ precache-manifest.2784414d703b42ac94b6ec44ca73eae5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c380cd86c17fc759fe8fca3409403e7",
+    "url": "/React-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ce37e8b69d14a2dd6ab0",
+    "url": "/React-monsters-rolodex/static/css/main.aab38c8b.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/React-monsters-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-monsters-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce37e8b69d14a2dd6ab0",
+    "url": "/React-monsters-rolodex/static/js/main.3caf1669.chunk.js"
+  },
+  {
+    "revision": "bad3ca086870b4b81352",
+    "url": "/React-monsters-rolodex/static/js/runtime-main.62957267.js"
+  }
+]);
